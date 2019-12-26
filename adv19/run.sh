ce8e2e01cfce8a5763b82ce6c2fc25177e96a530
Marcus <marcus.kindberg@protonmail.com>
2019-12-26 16:34:24 +0100

    adv19: Fix bug in run script

--- adv19/run.sh
+++ adv19/run.sh
@@ -2,6 +2,6 @@
 
 day=$1
 name=day${day}
-flags="-g -lm -o ${name} -Wall -Wextra -Werror"
+flags="-g -lm -o ${name}.out -Wall -Wextra -Werror"
 input=input.txt
 gcc day${day}.c ${flags} && ./${name}.out ${input}
