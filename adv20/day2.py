f64a118905e4085974a794a5d7b99e9795047741
Marcus <marcus.kindberg@protonmail.com>
2020-12-05 16:41:44 +0100

    adv20: use filter in day 2

--- adv20/day2.py
+++ adv20/day2.py
@@ -1,25 +1,37 @@
 import re
 import functools
 
-def validate(num_valid, string):
+def parse(string):
     pattern = "^(\d+)-(\d+) ([a-z]): ([a-z]+)$"
     m = re.match(pattern, string)
     low = int(m.group(1))
     high = int(m.group(2))
     letter = m.group(3)
     pwd = m.group(4)
+    return (low, high, letter, pwd)
+
+def rule1(data):
+    low = data[0]
+    high = data[1]
+    letter = data[2]
+    pwd = data[3]
 
     num = len(list(filter(lambda l: l == letter, pwd)))
-    if(num >= low and num <= high):
-        num_valid[0] += 1
-    if(pwd[low-1] == letter and pwd[high-1] != letter or
-       pwd[low-1] != letter and pwd[high-1] == letter):
-        num_valid[1] += 1
-    return num_valid
+    return num >= low and num <= high
 
+def rule2(data):
+    low = data[0]
+    high = data[1]
+    letter = data[2]
+    pwd = data[3]
+
+    return (pwd[low-1] == letter and pwd[high-1] != letter or
+           pwd[low-1] != letter and pwd[high-1] == letter)
 
 f = open("inp2.txt", "r")
-pwds = f.read().split("\n")
+pwds = f.read().split("\n")[:-1]
 f.close()
 
-print(functools.reduce(validate, pwds[:-1], [0, 0]))
+data = list(map(parse, pwds))
+print(len(list(filter(rule1, data))))
+print(len(list(filter(rule2, data))))
